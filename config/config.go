980bca33e651344833a46c2efc4e7eb61b6a6882
Kevin Kien <kevinkien@hotmail.com>
2025-05-07 00:05:01 +0700

    Create config.go

---
+++ config/config.go
@@ -0,0 +1,43 @@
+// config/config.go
+package config
+
+import "time"
+
+// Config holds all the configuration parameters for the blockchain
+type Config struct {
+	NumValidators      int
+	NumTransactions    int
+	BlockSize          int
+	ExecutionDelay     int
+	BlockInterval      time.Duration
+	QuorumPercentage   float64
+	SpeculativeEnabled bool
+}
+
+// DefaultConfig returns a configuration with sensible defaults
+func DefaultConfig() *Config {
+	return &Config{
+		NumValidators:      5,
+		NumTransactions:    20,
+		BlockSize:          5,
+		ExecutionDelay:     3, // D blocks execution delay
+		BlockInterval:      1 * time.Second,
+		QuorumPercentage:   0.67, // 2/3 of validators
+		SpeculativeEnabled: true,
+	}
+}
+
+// NewConfig creates a new configuration with the specified parameters
+func NewConfig(numValidators, numTransactions, blockSize, executionDelay int,
+	blockInterval time.Duration, quorumPercentage float64, speculativeEnabled bool) *Config {
+	
+	return &Config{
+		NumValidators:      numValidators,
+		NumTransactions:    numTransactions,
+		BlockSize:          blockSize,
+		ExecutionDelay:     executionDelay,
+		BlockInterval:      blockInterval,
+		QuorumPercentage:   quorumPercentage,
+		SpeculativeEnabled: speculativeEnabled,
+	}
+}
