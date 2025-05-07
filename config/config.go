6d4832abbdac1c1f82732754def646a278320a3c
Kevin Kien <kevinkien@hotmail.com>
2025-05-07 11:09:55 +0700

    Update config.go

--- config/config.go
+++ config/config.go
@@ -5,25 +5,31 @@ import "time"
 
 // Config holds all the configuration parameters for the blockchain
 type Config struct {
-	NumValidators      int
-	NumTransactions    int
-	BlockSize          int
-	ExecutionDelay     int
-	BlockInterval      time.Duration
-	QuorumPercentage   float64
-	SpeculativeEnabled bool
+	NumValidators          int
+	NumTransactions        int
+	BlockSize              int
+	ExecutionDelay         int
+	BlockInterval          time.Duration
+	QuorumPercentage       float64
+	SpeculativeEnabled     bool
+	ParallelExecutionEnabled bool
+	ParallelWorkers        int
+	OptimizeTransactionOrder bool
 }
 
 // DefaultConfig returns a configuration with sensible defaults
 func DefaultConfig() *Config {
 	return &Config{
-		NumValidators:      5,
-		NumTransactions:    20,
-		BlockSize:          5,
-		ExecutionDelay:     3, // D blocks execution delay
-		BlockInterval:      1 * time.Second,
-		QuorumPercentage:   0.67, // 2/3 of validators
-		SpeculativeEnabled: true,
+		NumValidators:          5,
+		NumTransactions:        20,
+		BlockSize:              5,
+		ExecutionDelay:         3, // D blocks execution delay
+		BlockInterval:          1 * time.Second,
+		QuorumPercentage:       0.67, // 2/3 of validators
+		SpeculativeEnabled:     true,
+		ParallelExecutionEnabled: true,
+		ParallelWorkers:        4,
+		OptimizeTransactionOrder: true,
 	}
 }
 
